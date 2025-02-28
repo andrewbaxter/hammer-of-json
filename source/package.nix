f624a3fd815963ac2ec853e988a9a43ac282d46d
Andrew Baxter <>
2025-02-28 21:27:21 +0900

    Nix

---
+++ source/package.nix
@@ -0,0 +1,7 @@
+{ pkgs, lib }:
+let
+  naersk = pkgs.callPackage (fetchTarball "https://github.com/nix-community/naersk/archive/378614f37a6bee5a3f2ef4f825a73d948d3ae921.zip") ();
+    in
+    naersk.buildPackage {
+    root = ./.;
+  }
